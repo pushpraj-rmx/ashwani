07813992ad8ebf7131b1e3bad27c7e3ca722af84
Developer's Community - NMP <161813137+DevCommNMP@users.noreply.github.com>
2024-07-04 14:00:12 +0530

    update

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBK7N677');</script>
+<!-- End Google Tag Manager -->
   <title>Ashwani Ayurveda Clinic</title>
   <!-- favicon icon -->
   <link rel="shortcut icon" href="https://nidmm.org/ashwani-ayurveda/assets/images/logo.png" type="image/x-icon" />
@@ -945,6 +952,11 @@
     <!-- main body end -->
   </div>
   <!-- body wrap end -->
+
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBK7N677"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
   <!-- Include jquery js -->
   <script src="assets/js/jquery.min.js"></script>
